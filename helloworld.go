1bb88d08069a9321e066e53110f26c05708ab4e9
ConnorBrereton <breretonconnor@gmail.com>
2020-04-10 17:15:55 -0700

    Exercise 1: print hello world

---
+++ helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world!")
+	fmt.Println("1+2 =", 1+2)
+	fmt.Println("42.0/2.0=" 42.0/2.0)
+	fmt.Println(true && false)
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
